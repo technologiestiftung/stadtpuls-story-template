93e8d1109c25dbf5c4cb76836962c4f3ae22ff76
Lucas Vogel <contact@vogelino.com>
2021-10-28 14:27:24 +0200

    feat: Use API call to get data and show chart

--- src/index.js
+++ src/index.js
@@ -41,3 +41,97 @@ new Chart(chart1Context, {
       },
     }
 });
+
+fetch('https://api.stadtpuls.com/api/v3/sensors/12/records')
+  .then((response) => response.json())
+  .then(({ data }) =>	drawChart2(data.slice(0, 200)))
+  .catch((err) => console.warn('Something went wrong.', err));
+
+function drawChart2(data) {
+  const tickStyles = {
+    grid: {
+      borderColor: '#f2f3f8',
+      tickColor: '#f2f3f8',
+      color: '#f2f3f8'
+    },
+    ticks: {
+      backdropColor: '#f2f3f8',
+      color: '#636188',
+      font: {
+        family: '"SpaceMono", monospace',
+      },
+    },
+  };
+  const chart2Context = document.getElementById('chart2').getContext('2d');
+  new Chart(chart2Context, {
+      type: 'line',
+      data: {
+        labels: data.map(({ recorded_at }) => new Date(recorded_at).toLocaleString('de-DE', {
+          hour: '2-digit',
+          minute: '2-digit',
+          day: '2-digit',
+          month: 'long',
+          year: '2-digit',
+        })),
+        datasets: [{
+          data: data.map(({ measurements }) => measurements[0]),
+          borderColor: '#8330ff',
+          borderWidth: 1,
+          fill: false,
+          tension: 0.1
+        }]
+      },
+      options: {
+        elements: {
+          point: {
+            radius: 1.5,
+            backgroundColor: '#8330ff',
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            usePointStyle: true,
+            backgroundColor: '#100c53',
+            padding: 16,
+            cornerRadius: 0,
+            titleFont: {
+              family: 'SpaceGrotesk',
+            },
+            bodyFont: {
+              family: 'SpaceMono',
+            },
+            callbacks: {
+              labelColor: () => ({
+                backgroundColor: '#8330ff',
+                borderColor: '#8330ff',
+                borderWidth: 1,
+              }),
+            }
+          }
+        },
+        interaction: {
+          mode: 'index',
+          intersect: false
+        },
+        scales: {
+          y: tickStyles,
+          x: {
+            ...tickStyles,
+            ticks: {
+              ...tickStyles.ticks,
+              callback: function(value, index, values) {
+                const label = new Date(data[index].recorded_at).toLocaleString('de-DE', {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                });
+                return index % 4 === 0 ? label : ''
+              }
+            }
+          }
+        }
+      }
+  });
+}
