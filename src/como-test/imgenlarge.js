83fd013b680a1b4281956c2af0c898736524c1d6
annameide <46717848+annameide@users.noreply.github.com>
2022-09-20 16:30:25 +0200

    fix: tryout of fixing the img enlargement with fabian

--- src/como-test/imgenlarge.js
+++ src/como-test/imgenlarge.js
@@ -1,20 +1,25 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM fully loaded and parsed');
+  // Get the modal
+  var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('img2');
-var modalImg = document.getElementById('img02');
-var captionText = document.getElementById('caption');
-img.onclick = function () {
-  modal.style.display = 'block';
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-};
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById('img2');
+  var modalImg = document.getElementById('img02');
+  var captionText = document.getElementById('caption');
+  img.addEventListener('click', function () {
+    console.log('clicked img');
+    modal.classList.add('display_block');
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close')[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+  // When the user clicks on <span> (x), close the modal
+
+  span.addEventListener('click', function () {
+    modal.classList.remove('display_block');
+  });
+});
